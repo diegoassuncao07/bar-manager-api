13bef209cc9a09434606bc030e597e00059bfb21
Diego <diego_assuncao07@outlook.com>
2026-05-27 00:04:32 -0300

    feat: initial project setup with Spring Boot 3.5.14

---
+++ src/main/java/com/diegoassuncao/barmanager/BarmanagerApplication.java
@@ -0,0 +1,13 @@
+package com.diegoassuncao.barmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BarmanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BarmanagerApplication.class, args);
+	}
+
+}
